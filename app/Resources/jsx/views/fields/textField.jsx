0839952f134b4870eb7660da90b3b9251920ab32
Joonas Kaskisola <joonas.kaskisola@protonmail.com>
2016-11-17 03:02:23 +0200

    wip

--- app/Resources/jsx/views/fields/textField.jsx
+++ app/Resources/jsx/views/fields/textField.jsx
@@ -6,7 +6,7 @@ export default class TextField extends React.Component {
         return <div className="row-column">
             <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                 {this.props.label}: <input
-                value={this.props.value}
+                value={this.props.value ? this.props.value : ''}
                 className="mdl-textfield__input"
                 type="text" id={this.props.name} name={this.props.name}
                 onChange={this.props.handleChange}/>
