c7ef8da1673708ad70c4c58c2b6859e8ef370baf
Joonas Kaskisola <joonaskaskisola@gmail.com>
2017-09-10 19:16:27 +0300

    added aws conf

---
+++ conf/insert-configs-aws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export APP_SECRET=seg098ue35t93y7t83y4otr4w3
+export BONSAI_URL=https://user:pass@bonsaisearch
+export CLEARDB_DATABASE_URL=mysql://_REPLACE_MYSQL_USER_:_REPLACE_MYSQL_PASSWD_@_REPLACE_MYSQL_HOST_/taskmanager
+
+export MEMCACHEDCLOUD_PASSWORD=
+export MEMCACHEDCLOUD_SERVERS=_REPLACE_MEMCACHED_HOST_:11211
+export MEMCACHEDCLOUD_USERNAME=
+
+export RABBITMQ_BIGWIG_REST_API_URL=https://user:pass@host/management/123456/api
+export RABBITMQ_BIGWIG_RX_URL=amqp://user:pass@host:port/app
+export RABBITMQ_BIGWIG_TX_URL=amqp://user:pass@host:port/app
+export RABBITMQ_BIGWIG_URL=amqp://user:pass@host:port/app
+export CLOUDINARY_URL=cloudinary://user:pass@host
+
+export SYMFONY_ENV=prod
